d578cd8d241316415cfdbad9d3110515f7093cce
Ben Shaker <bshaker@vt.edu>
2019-04-29 21:43:54 -0400

    google cloud vision responding with scores & tags

--- indoor.py
+++ indoor.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 # indoor.py
 
+from indoor_keys import CloudVisionKey
+import requests
+
+# example request below
+#   note that content must be a base64 string
+#   starting with /9j/ or whatever
+
+# {
+#   "requests":[
+#     {
+#       "image":{
+#         "content":"/9j/7QBEUGhvdG9...image contents...eYxxxzj/Coa6Bax//Z"
+#       },
+#       "features":[
+#         {
+#           "type":"LABEL_DETECTION",
+#           "maxResults":1
+#         }
+#       ]
+#     }
+#   ]
+# }
+
+r = requests.post(
+    'https://vision.googleapis.com/v1/images:annotate?key='
+     + CloudVisionKey,
+    json=dataz)
+json_res = r.json()
+responses = json_res['responses']
+annotations = responses[0]['labelAnnotations']
+
+for annote in annotations:
+    print(annote['description'], annote['score'])
