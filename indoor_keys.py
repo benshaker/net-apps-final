e0018abd21f9462cb2f676975f87aa4635b33a48
Ben Shaker <bshaker@vt.edu>
2019-04-29 20:12:39 -0400

    initial commit

---
+++ indoor_keys.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# indoor_keys.py
+
+CloudVisionKey = "ya29.c.El_7Br-qsS9YlSrjuNueQXCeWAno3wnSa9dIpIIn9dfm2-7z8zV05TTQ2El0WBJ68UVb72oWW8jABCdJE0nmHI3bBuaPHMKtbwR_92R6Y-rzCB9YSUQH7-JrUHbWKG3X9Q"
