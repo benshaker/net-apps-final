788b6651e24ce67f696fc6f85bf3bb13a10aea6a
benshaker <bshaker@vt.edu>
2019-05-06 14:54:58 -0400

    ignoring order of mongo entries

--- static/js/settings.js
+++ static/js/settings.js
@@ -8,54 +8,45 @@
 
   function reqListener () {
     res_arr   = JSON.parse(this.responseText);
-    for (var i = res_arr.length - 1; i >= 0; i--) {
+    //console.log(res_arr);
+    for (var i = 3; i >= 0; i--) {
       name = res_arr[i]["name"];
-
+      //console.log(res_arr[i]);
       if(name.includes('time')){
+	//console.log('true');
         val = JSON.stringify(res_arr[i][name]);
       }
       else val = res_arr[i][name];
+      //console.log(name);
+      //console.log(val);
 
       document.getElementById(name).value = val;
     };
-    // console.log(res_arr[0]["whitelist"])
-    // console.log(res_arr[1]["blacklist"])
-    // console.log(res_arr[2]["daytime"])
-    // console.log(res_arr[3]["nighttime"])
-    // whitelist = res_arr[0]["whitelist"];
-    // blacklist = res_arr[1]["blacklist"];
-    // daytime   = JSON.stringify(res_arr[2]["daytime"]);
-    // nighttime = JSON.stringify(res_arr[3]["nighttime"]);
-
-    // document.getElementById("white").value = whitelist;
-    // document.getElementById("black").value = blacklist;
-    // document.getElementById("day").value = daytime;
-    // document.getElementById("night").value = nighttime;
   }
 
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("load", reqListener);
-  xhr.open("GET", "http://localhost:8080/load_settings");
+  xhr.open("GET", "/load_settings");
   xhr.send();
 
   function RespondClick() {
-      whitelist = document.getElementById("white").value.split(",");
+      whitelist = document.getElementById("whitelist").value.split(",");
       whitelist = {"whitelist":whitelist, "name":"whitelist"}
       // console.log(whitelist)
-      blacklist = document.getElementById("black").value.split(",");
+      blacklist = document.getElementById("blacklist").value.split(",");
       blacklist = {"blacklist":blacklist, "name":"blacklist"}
       // console.log(blacklist)
-      daytime   = document.getElementById("day").value.split(",");
+      daytime   = document.getElementById("daytime").value.split(",");
       daytime = {"daytime":JSON.parse(daytime), "name":"daytime"}
       // console.log(daytime)
-      nighttime = document.getElementById("night").value.split(",");
+      nighttime = document.getElementById("nighttime").value.split(",");
       nighttime = {"nighttime":JSON.parse(nighttime), "name":"nighttime"}
       // console.log(nighttime)
 
       data = {'data': [whitelist, blacklist, daytime, nighttime]}
 
       var xhr = new XMLHttpRequest();
-      xhr.open("PUT", "http://localhost:8080/save_settings");
+      xhr.open("PUT", "/save_settings");
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.send(JSON.stringify(data));
   }
