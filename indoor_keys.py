d5d0ca0fa294403f68cf9441e71c09557ada7c97
Ben <bshaker@vt.edu>
2019-05-11 17:18:28 -0400

    Update indoor_keys.py

--- indoor_keys.py
+++ indoor_keys.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # indoor_keys.py
 
-CloudVisionKey = "AIzaSyDYZO9zkj4ms44NZuMc4b-3WmiKlfTQkL0"
+CloudVisionKey = "AIzaSyDYZO9zkj4ms44NZuMc4b-3WmiKlfTQkL0" # this key has been deactivated
